00744a9453691228e0adb2a1ab3b09b6eab7c912
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-08-20 20:23:40 +0530

    Update page.jsx

--- app/questions/[slug]/page.jsx
+++ app/questions/[slug]/page.jsx
@@ -67,7 +67,7 @@ const Page = ({ params }) => {
       </h1>
       {/* Verified Solution */}
       <div className="bg-gray-100 p-4 rounded-lg shadow-md mb-3">
-        <p className="text-sm text-gray-500">Verified by Guideblog</p>
+        <p className="text-sm text-gray-500">Verified by Examgain</p>
         <p className="mt-2 text-base">Answer to this question is verified!</p>
       </div>
       <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4 mb-8">
