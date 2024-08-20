1042d876609128f2911b0e95e1a30631e2c9d250
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-08-20 20:21:58 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
       <div className="container mx-auto flex items-center justify-between">
         {/* Logo on the left */}
         <div className="text-2xl font-bold">
-          <a href="/">Guideblog</a>
+          <a href="/">Examgain</a>
         </div>
 
         {/* Hamburger icon for mobile */}
