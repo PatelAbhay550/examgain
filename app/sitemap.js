f796e515ba69d6bc7f250014f6293f84742c73c7
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-08-27 12:07:56 +0530

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,6 +1,22 @@
 import { suggestions } from "@/data/suggestions";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "@/lib/firebaseconfig";
 
-export default function sitemap() {
+export default async function sitemap() {
+  const q = query(collection(db, "Notes"));
+
+  // Fetch the posts from Firestore
+  const querySnapshot = await getDocs(q);
+  const blogPosts = [];
+
+  querySnapshot.forEach((doc) => {
+    blogPosts.push({
+      id: doc.id,
+      slug: doc.data().slug, // Make sure 'slug' is a field in your document
+      ...doc.data(),
+    });
+  });
+
   const staticUrls = [
     {
       url: "https://examgain.vercel.app",
@@ -28,6 +44,12 @@ export default function sitemap() {
     changeFrequency: "weekly",
     priority: 1,
   }));
+  const NotesUrls = blogPosts.map((item) => ({
+    url: `https://examgain.vercel.app/guides/notes/${item.category}/${item.slug}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 1,
+  }));
 
-  return [...staticUrls, ...questionUrls];
+  return [...staticUrls, ...questionUrls, ...NotesUrls];
 }
