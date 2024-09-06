9d234bfc58df5565a970e8f6339b5eb3c8f74e16
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-09-06 15:33:12 +0530

    Create revision.js

---
+++ data/revision.js
@@ -0,0 +1,389 @@
+export const revision = [
+  {
+    level: "National Parks",
+    topic: "SSC CGL",
+    totalQuestions: 10,
+    perQuestionScore: 2,
+    questions: [
+      {
+        question: "Tadoba national is located in",
+        choices: ["Maharastra", "Uttar Prasesh", "Tamil Nadu", "Uttarakhand"],
+        type: "MCQs",
+        correctAnswer: "Maharastra",
+        extradetails:
+          "  Tadoba National Park is located in Chandrapur district of Maharastra. It is the largest park of Maharastra, and oldest National Park.",
+      },
+      {
+        question: "The Silent Valley National Park is in",
+        choices: ["Uttarakhand", "Odisha", "Himachal Pradesh", "Kerala"],
+        type: "MCQs",
+        correctAnswer: "Kerala",
+        extradetails:
+          "Silent Valley National Park is located in the Nilgiri Hills in Kerala, India. It was established on 15 November 1984.",
+      },
+      {
+        question: "The Pin Valley National Park is located in",
+        choices: ["Uttarakhand", "Odisha", "Himachal Pradesh", "Kerala"],
+        type: "MCQs",
+        correctAnswer: "Himachal Pradesh",
+        extradetails:
+          "Pin Valley National Park is located in Lahaul and Spiti district of Himachal Pradesh. It is famous for endangered Snow leopards. Pin Valley National Park was established in 1958.",
+      },
+      {
+        question:
+          "Which of these is the National Park located in Uttar Pradesh",
+        choices: [
+          "Mollem National Park",
+          "Dudhwa National Park",
+          "Bandipur National Park",
+          "Sariska National Park",
+        ],
+        type: "MCQs",
+        correctAnswer: "Dudhwa National Park",
+        extradetails:
+          "Dudhwa National Park is located in Uttar Pradesh's Kheri and Lakhimpur Districts. It was established in 1977 as a part of Dushwa Tiger reserve.",
+      },
+      {
+        question: "Sariska National Park is located in",
+        choices: ["Maharastra", "Odisha", "Manipur", "Rajasthan"],
+        type: "MCQs",
+        correctAnswer: "Rajasthan",
+        extradetails:
+          "Sariska Tiger Reserve is a National Park located in Alwar District of Rajasthan.",
+      },
+      {
+        question: "Identify the National Park located in Jammu and Kashmir",
+        choices: [
+          "Salim Ali National Park",
+          "Hemis National Park",
+          "Bandipur National Park",
+          "Dudhwa",
+        ],
+        type: "MCQs",
+        correctAnswer: "Salim Ali National Park",
+        extradetails:
+          "Salim Ali national Park is located in Srinagar, Jammu and Kashmir. Salim ali national Park is also known as City forest national park. It is named after Indian Ornithologist and Bird-Lover Salim Ali.",
+      },
+      {
+        question:
+          "Largest National Park, i.e. Hemis National Park is Located in",
+        choices: ["Arunachal Padesh", "Uttarakhand", "Laddakh", "Assam"],
+        type: "MCQs",
+        correctAnswer: "Laddakh",
+        extradetails:
+          "Hemis national Park, located in Leh District of Laddakh is the largest National Park of India. Hemis National Park is famous for Snow leopards and was established in 1981.",
+      },
+      {
+        question: "Kuchenjuga National Park is located in",
+        choices: ["Maharastra", "Sikkim", "Manipur", "Assam"],
+        type: "MCQs",
+        correctAnswer: "Sikkim",
+        extradetails:
+          "Kunchenjunga national park is located in Sikkim. It is also called Kunchenjunga Biosphere Reserve. It is the first Mixed Heritage site in UNESCO World Heritage sites list. Kunchenjunga National Park was established in 1977.",
+      },
+      {
+        question: "National Park located in Uttarakhand is",
+        choices: [
+          "Panna National Park",
+          "Nanda Devi National Park",
+          "Mollem National Park",
+          "Hemis National Park",
+        ],
+        type: "MCQs",
+        correctAnswer: "Nanda Devi National Park",
+        extradetails:
+          "Nanda Devi National Park is located in Chamoli Garhwal district of Uttarakhand. Nanda Devi National Park was Established in 1982.",
+      },
+      {
+        question: "Largest National Park in Madhya Pradesh is",
+        choices: [
+          "Kanha Tiger Reserve",
+          "Satpura National Park",
+          "Pench National Park",
+          "Van Vihar National Park",
+        ],
+        type: "MCQs",
+        correctAnswer: "Kanha Tiger Reserve",
+        extradetails:
+          "Kanha Tiger reserve is a national Park located in Madhya Pradesh. It has Tigers, jackals, and wild pigs. It was established in 1955",
+      },
+    ],
+  },
+  {
+    level: "River System",
+    topic: "SSC CGL",
+    totalQuestions: 10,
+    perQuestionScore: 2,
+    questions: [
+      {
+        question: "The Ganges River originates from which location?",
+        choices: [
+          "Kailash Range",
+          "Yamunotri Glacier",
+          "Gangotri Glacier",
+          "Satopanth Glacier",
+        ],
+        type: "MCQs",
+        correctAnswer: "Gangotri Glacier",
+        extradetails:
+          "The Ganges River originates as Bhagirathi river from the Gangotri Glacier in the Himalayas.When Bhagirathi river meets Alaknanda river at Dev Paryag, the combined river is known as Ganga. Its length is 2525km. It is one of the most important rivers in India.",
+      },
+      {
+        question: "Which river is known as 'Sorrow of Bihar'?",
+        choices: ["Ganges", "Yamuna", "Kosi", "Brahmaputra"],
+        type: "MCQs",
+        correctAnswer: "Kosi",
+        extradetails:
+          "The Kosi River is known as the 'Sorrow of Bihar' due to its frequent flooding, which causes significant damage to the region.",
+      },
+      {
+        question:
+          "The Brahmaputra River is primarily associated with which region?",
+        choices: ["Assam Valley", "Punjab", "Ganges Delta", "Western Ghats"],
+        type: "MCQs",
+        correctAnswer: "Assam Valley",
+        extradetails:
+          "The Brahmaputra River flows through the Assam Valley and is a major river in the northeastern region of India. Brahmaputra river originates from lake Mansarovar in Tibbet where it is called Tsangpo.",
+      },
+      {
+        question: "Which river is the longest in Peninsular India?",
+        choices: ["Godavari", "Yamuna", "Ganges", "Brahmaputra"],
+        type: "MCQs",
+        correctAnswer: "Godavari",
+        extradetails:
+          "The Godavari River is the longest peninsular river in India, flowing across central and southern India.",
+      },
+      {
+        question: "The Yamuna River is a tributary of which major river?",
+        choices: ["Ganges", "Godavari", "Brahmaputra", "Kaveri"],
+        type: "MCQs",
+        correctAnswer: "Ganges",
+        extradetails:
+          "The Yamuna River is a major tributary of the Ganges River and flows through northern India. It originates from Yamunotri Glacier in Uttarakhnad.",
+      },
+      {
+        question:
+          "Which river forms the border between India and Pakistan in the Punjab region?",
+        choices: ["Chenab", "Jhelum", "Ravi", "Sutlej"],
+        type: "MCQs",
+        correctAnswer: "Chenab",
+        extradetails:
+          "The Chenab River forms part of the border between India and Pakistan in the Punjab region.",
+      },
+      {
+        question: "The Narmada River flows into which body of water?",
+        choices: [
+          "Bay of Bengal",
+          "Arabian Sea",
+          "Indian Ocean",
+          "Andaman Sea",
+        ],
+        type: "MCQs",
+        correctAnswer: "Arabian Sea",
+        extradetails:
+          "The Narmada River flows westward and empties into the Arabian Sea.",
+      },
+      {
+        question: "Which river is known as 'Dakshina Ganga'?",
+        choices: ["Krishna", "Kaveri", "Godavari", "Mahanadi"],
+        type: "MCQs",
+        correctAnswer: "Godavari",
+        extradetails:
+          "The Godavari River is often referred to as the 'Dakshina Ganga' or 'Vriddh Ganga' due to its significance in southern India.",
+      },
+      {
+        question: "Which river is the main source of the Indus River?",
+        choices: ["Zanskar", "Jhelum", "Chenab", "Ravi"],
+        type: "MCQs",
+        correctAnswer: "Zanskar",
+        extradetails:
+          "The Zanskar River is one of the main sources of the Indus River, originating from the Tibetan Plateau.",
+      },
+      {
+        question: "Which river is famous for its delta, the Sundarbans?",
+        choices: ["Sabarmati", "Brahmaputra", "Godavari", "Kaveri"],
+        type: "MCQs",
+        correctAnswer: "Brahmaputra",
+        extradetails:
+          "The Brahmaputra River forms the Sundarbans delta, which is known for its rich biodiversity and mangrove forests.",
+      },
+    ],
+  },
+  {
+    level: "Quick Revision 01",
+    topic: "SSC CGL",
+    totalQuestions: 10,
+    perQuestionScore: 2,
+    questions: [
+      {
+        question: "Who was the founder of the Maurya Dynasty?",
+        choices: ["Ashoka", "Chandragupta Maurya", "Bindusara", "Kanishka"],
+        type: "MCQs",
+        correctAnswer: "Chandragupta Maurya",
+      },
+      {
+        question: "Which state in India is known as the 'Land of Five Rivers'?",
+        choices: ["Uttar Pradesh", "Punjab", "Haryana", "Bihar"],
+        type: "MCQs",
+        correctAnswer: "Punjab",
+      },
+      {
+        question:
+          "The President of India is elected for a term of how many years?",
+        choices: ["4 years", "5 years", "6 years", "7 years"],
+        type: "MCQs",
+        correctAnswer: "5 years",
+      },
+      {
+        question:
+          "If a sum of Rs. 5000 amounts to Rs. 6500 in 2 years at simple interest, what is the rate of interest per annum?",
+        choices: ["10%", "12%", "15%", "20%"],
+        type: "MCQs",
+        correctAnswer: "15%",
+      },
+      {
+        question:
+          "In a certain code language, 'BRAIN' is written as 'AQZHM'. How will 'HEART' be written in the same code?",
+        choices: ["GDZQS", "GDSZQ", "GDRZS", "GDSRQ"],
+        type: "MCQs",
+        correctAnswer: "GDZQS",
+      },
+      {
+        question: "Choose the correct synonym for the word 'Inevitable'.",
+        choices: ["Avoidable", "Optional", "Unavoidable", "Doubtful"],
+        type: "MCQs",
+        correctAnswer: "Unavoidable",
+      },
+      {
+        question:
+          "Which element is essential for the formation of bones and teeth?",
+        choices: ["Iron", "Calcium", "Potassium", "Sodium"],
+        type: "MCQs",
+        correctAnswer: "Calcium",
+      },
+      {
+        question:
+          "Which battle led to the establishment of British rule in India?",
+        choices: [
+          "Battle of Plassey",
+          "Battle of Panipat",
+          "Battle of Haldighati",
+          "Battle of Buxar",
+        ],
+        type: "MCQs",
+        correctAnswer: "Battle of Plassey",
+      },
+      {
+        question: "Which mountain range separates Europe from Asia?",
+        choices: ["Alps", "Himalayas", "Ural", "Rockies"],
+        type: "MCQs",
+        correctAnswer: "Ural",
+      },
+      {
+        question: "What is the chemical formula for common salt?",
+        choices: ["HCl", "NaCl", "KCl", "CaCl2"],
+        type: "MCQs",
+        correctAnswer: "NaCl",
+      },
+    ],
+  },
+  {
+    level: "Quick Revision 02",
+    topic: "SSC CGL",
+    totalQuestions: 10,
+    perQuestionScore: 2,
+    questions: [
+      {
+        question: "Who among the following introduced the Doctrine of Lapse?",
+        choices: [
+          "Lord Canning",
+          "Lord Dalhousie",
+          "Lord Ripon",
+          "Lord Curzon",
+        ],
+        type: "MCQs",
+        correctAnswer: "Lord Dalhousie",
+      },
+      {
+        question:
+          "Which Indian state shares the longest border with Bangladesh?",
+        choices: ["Assam", "West Bengal", "Tripura", "Meghalaya"],
+        type: "MCQs",
+        correctAnswer: "West Bengal",
+      },
+      {
+        question:
+          "Which Article of the Indian Constitution deals with the protection of life and personal liberty?",
+        choices: ["Article 19", "Article 20", "Article 21", "Article 22"],
+        type: "MCQs",
+        correctAnswer: "Article 21",
+      },
+      {
+        question: "What is the square root of 1444?",
+        choices: ["36", "38", "40", "42"],
+        type: "MCQs",
+        correctAnswer: "38",
+      },
+      {
+        question:
+          "If A is coded as Z, B as Y, and so on, how is the word 'CODE' written in that code?",
+        choices: ["XLMW", "XKHV", "XLWV", "XLHV"],
+        type: "MCQs",
+        correctAnswer: "XLWV",
+      },
+      {
+        question:
+          "Identify the incorrect part of the following sentence: 'The team have performed excellently throughout the tournament.'",
+        choices: [
+          "The team",
+          "have performed",
+          "excellently",
+          "throughout the tournament",
+        ],
+        type: "MCQs",
+        correctAnswer: "have performed",
+      },
+      {
+        question:
+          "Which part of the human brain is responsible for balance and coordination?",
+        choices: [
+          "Cerebrum",
+          "Cerebellum",
+          "Medulla Oblongata",
+          "Hypothalamus",
+        ],
+        type: "MCQs",
+        correctAnswer: "Cerebellum",
+      },
+      {
+        question:
+          "The Treaty of Alinagar was signed in 1757 between the British East India Company and whom?",
+        choices: [
+          "Tipu Sultan",
+          "Siraj ud-Daulah",
+          "Nawab of Oudh",
+          "Marathas",
+        ],
+        type: "MCQs",
+        correctAnswer: "Siraj ud-Daulah",
+      },
+      {
+        question: "Which ocean is the smallest in the world?",
+        choices: [
+          "Indian Ocean",
+          "Arctic Ocean",
+          "Atlantic Ocean",
+          "Southern Ocean",
+        ],
+        type: "MCQs",
+        correctAnswer: "Arctic Ocean",
+      },
+      {
+        question: "Which vitamin is known as ascorbic acid?",
+        choices: ["Vitamin A", "Vitamin B", "Vitamin C", "Vitamin D"],
+        type: "MCQs",
+        correctAnswer: "Vitamin C",
+      },
+    ],
+  },
+];
